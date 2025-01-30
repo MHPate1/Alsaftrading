584ffe250db04c667f4af9a6f34cf1c067e627fd
Muhammad <muhammadpatel465@gmail.com>
2025-01-30 17:59:40 +0000

    fixing error on page issue

--- perfume_store/perfume_store/settings.py
+++ perfume_store/perfume_store/settings.py
@@ -15,13 +15,13 @@ ALLOWED_HOSTS = [
     'giftoutlet.co.uk',
     'www.giftoutlet.co.uk',
     'alsaftrading-900247810812.europe-west1.run.app',
-    '*'  # Temporarily add this for testing
+    '*',  # Temporarily add this for testing
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://giftoutlet.co.uk',
     'https://www.giftoutlet.co.uk',
-    'https://alsaftrading-900247810812.europe-west1.run.app'
+    'https://alsaftrading-900247810812.europe-west1.run.app',
 ]
 
 # STRIPE SETTINGS
@@ -39,10 +39,11 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
-SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
-X_FRAME_OPTIONS = 'DENY'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
